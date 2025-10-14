93e44e97775203c4ae48f85822d597723dd61423
RtnMDRam <rtnmdramamoorthy@gmail.com>
2025-10-14 10:43:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,27 @@
-import streamlit as st
+import matplotlib.pyplot as plt
 
-st.set_page_config(layout="wide", initial_sidebar_state="collapsed")
+# Dimensions
+width = 1.0
+height = 1.0
 
-st.markdown("""
-<style>
-.outer-panel-border {
-    position: fixed;
-    top: 6vh;
-    left: 1vw;
-    width: 98vw;
-    height: 91vh;
-    border: 4px solid #2a3a88;
-    border-radius: 18px;
-    background: #fff;
-    box-sizing: border-box;
-    z-index: 99;
-}
-</style>
-<div class="outer-panel-border"></div>
-""", unsafe_allow_html=True)
+# Fixed outer border (do not change)
+fig, ax = plt.subplots(figsize=(6,8))
+ax.plot([0, width], [0, 0], color='navy')       # bottom border
+ax.plot([0, width], [height, height], color='navy') # top border
+ax.plot([0, 0], [0, height], color='navy')      # left border
+ax.plot([width, width], [0, height], color='navy')  # right border
+
+# Partition percentages (must sum to 1 or 100%)
+part_percentages = [0.03, 0.04, 0.08, 0.45, 0.20, 0.20]
+
+# Draw partition lines at cumulative positions
+y_positions = [sum(part_percentages[:i+1]) * height for i in range(len(part_percentages))]
+
+for y in y_positions[:-1]:  # skip last; it's the bottom border
+    ax.plot([0, width], [y, y], color='red')
+
+ax.set_xlim(-0.05, 1.05)
+ax.set_ylim(-0.05, 1.05)
+ax.set_xticks([])
+ax.set_yticks([])
+plt.show()
