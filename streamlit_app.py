53c6ad7b291c71ea1cb1beca8ad9d40dd2dd8bbb
RtnMDRam <rtnmdramamoorthy@gmail.com>
2025-10-14 09:58:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,24 +4,18 @@ st.set_page_config(layout="centered", initial_sidebar_state="collapsed")
 
 st.markdown("""
 <style>
-.outer-panel-border-wrapper {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100vw;
-    height: 100vh;
-    box-sizing: border-box;
-}
 .outer-panel-border {
-    width: 90vw;
-    height: 85vh;
+    position: fixed;
+    top: 2vh;
+    left: 2vw;
+    width: 96vw;
+    height: 96vh;
     border: 4px solid #2a3a88;
     border-radius: 18px;
     background: #fff;
     box-sizing: border-box;
+    z-index: 99;
 }
 </style>
-<div class="outer-panel-border-wrapper">
-  <div class="outer-panel-border"></div>
-</div>
+<div class="outer-panel-border"></div>
 """, unsafe_allow_html=True)
