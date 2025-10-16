5868141069ced04cefce9119596aaa002cf39dbe
RtnMDRam <rtnmdramamoorthy@gmail.com>
2025-10-16 08:30:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ authenticator = stauth.Authenticate(
 )
 
 # Only these two values are valid: 'main' or 'sidebar'
-name, authentication_status, username = authenticator.login('Login', 'unrendered')
+name, authentication_status, username = authenticator.login('Login', 'main')
 
 if authentication_status:
     st.success(f'Welcome {name}')  # Optionally, replace with your branding
